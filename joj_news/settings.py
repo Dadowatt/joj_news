6ccd33c5637804c364e54d6a3df3265d3b3c8ca1
Dadowatt <dadowatt92@gmail.com>
2026-04-08 21:37:30 +0000

    dernière fonctionnalité terminée

--- joj_news/settings.py
+++ joj_news/settings.py
@@ -70,14 +70,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'joj_news.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'joj_news_db',
+        'USER': 'dado',
+        'PASSWORD': 'Watt1234!',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
